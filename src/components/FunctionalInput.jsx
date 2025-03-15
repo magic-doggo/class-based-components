3f96fee11351a379cae5b7a1678f154f13083dae
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-03-15 13:18:07 +0000

    Implement the functionality of the edit and save changes buttons finished tasks on functionalInput component

--- src/components/FunctionalInput.jsx
+++ src/components/FunctionalInput.jsx
@@ -4,19 +4,24 @@ import Count from './Count';
 // eslint-disable-next-line react/function-component-definition, react/prop-types
 const FunctionalInput = ({ name }) => {
 
-  const [todos, setTodos] = useState([{ task: 'Just some demo tasks', isCurrentlyEditing: false },
-  { task: 'As an example', isCurrentlyEditing: false }]);
+  const [todos, setTodos] = useState([{ task: 'Just some demo tasks', isCurrentlyEditing: false, id: 1 },
+  { task: 'As an example', isCurrentlyEditing: false, id: 2 }]);
   const [inputVal, setInputVal] = useState('');
-
+  const [nextId, setNextId] = useState(3)
   const handleInputChange = (e) => {
     setInputVal(e.target.value);
   };
 
+  const [editedValue, setEditedValue] = useState('');
+  const handleEditedChange = (e) => {
+    setEditedValue(e.target.value);
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // setTodos((todo) => [...todo, inputVal]);
-    setTodos((todo) => [...todo, { task: inputVal, isCurrentlyEditing: false }]);
+    setTodos((todo) => [...todo, { task: inputVal, isCurrentlyEditing: false, id: nextId }]);
     setInputVal('');
+    setNextId(nextId + 1)
   };
 
   return (
@@ -36,38 +41,35 @@ const FunctionalInput = ({ name }) => {
       <h4>All the tasks!</h4>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.task}>
-            {todo.isCurrentlyEditing ? (<input type='text' />) : (<div>{todo.task} aaaa</div>)}
-            {/* <div>{todo.task} aaaa</div> */}
+          <li key={todo.id}>
+            {todo.isCurrentlyEditing ? (
+              <input type='text' value={editedValue} onChange={handleEditedChange}/>
+              ) : (<div>{todo.task}</div>)}
+            {/* make the above editable. probably need some id for key or some way to access state to set it as value above
+            or create new state. and assign it the value of todo.task 
+            easier if can only edit one submission at a time. pressing edit should close other edits (set their iscurrentlyediting to true,
+            and either discard or submit changes. probably submit*/}
 
             {todo.isCurrentlyEditing ? (
               <button onClick={() => {
                 setTodos(todos.map(entry => {
-                  if (entry.task == todo.task) {
-                    return { task: entry.task, isCurrentlyEditing: false }
+                  if (entry.id == todo.id) {
+                    return { ...entry, isCurrentlyEditing: false, task:editedValue }
                   } else return entry
                 }))
-              }}>Submit</button>) : (
+              }}>Save Change</button>) : (
                 <button onClick={() => {
+                  setEditedValue(todo.task)
                   setTodos(todos.map(entry => {
-                    if (entry.task == todo.task) {
-                      return { task: entry.task, isCurrentlyEditing: true }
+                    if (entry.id == todo.id) {
+                      return { ...entry, isCurrentlyEditing: true }
                     } else return entry
                   }))
                 }}>Edit</button>    
             )}
 
-            {/* <button onClick={() => {
-              setTodos(todos.map(entry => {
-                if (entry.task == todo.task) {
-                  console.log("asdddddddd");
-                  return { task: entry.task, isCurrentlyEditing: true }
-                } else return entry
-              }))
-            }}>Edit</button> */}
-
             <button onClick={() => {
-              setTodos(todos.filter(entry => entry.task !== todo.task))
+              setTodos(todos.filter(entry => entry.id !== todo.id))
             }}>Delete</button>
           </li>
         ))}
